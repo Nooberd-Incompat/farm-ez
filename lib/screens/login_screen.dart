5a2a8ba56011086fd892b529c3c47adb2c23e704
Yojith Kaustabh <yojith212@gmail.com>
2024-03-08 16:07:43 +0530

    improved login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,22 @@
+import 'package:farm_ez/screens/home_screen.dart';
 import 'package:farm_ez/widgets/custom_clipper.dart';
 import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+// import 'package:firebase_storage/firebase_storage.dart';
 
+// final _firebase = FirebaseAuth.instance;
+
+// class AuthScreen extends StatefulWidget {
+//   const AuthScreen({super.key});
+
+//   @override
+//   State<AuthScreen> createState() {
+//     return _AuthScreenState();
+//   }
+// }
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -12,11 +28,79 @@ class _LoginScreenState extends State<LoginScreen> {
   late TextEditingController emailController;
   late TextEditingController passwordController;
 
+  final _formKey = GlobalKey<FormState>();
+
+  var _isLogin = true;
+  var _enteredEmail = "";
+  var _enteredPassword = "";
+  var _isAuthenticating = false;
+  var _enteredUsername = "";
+
+  void _submit() async {
+    final isValid = _formKey.currentState!.validate();
+
+    if (!isValid || !_isLogin) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+
+    // try {
+    //   setState(() {
+    //     _isAuthenticating = true;
+    //   });
+    //   if (_isLogin) {
+    //     final userCredentials = await _firebase.signInWithEmailAndPassword(
+    //         email: _enteredEmail, password: _enteredPassword);
+    //   } else {
+    //     final userCredentials = await _firebase.createUserWithEmailAndPassword(
+    //         email: _enteredEmail, password: _enteredPassword);
+
+    //     final storageRef = FirebaseStorage.instance
+    //         .ref()
+    //         .child('user_images')
+    //         .child('${userCredentials.user!.uid}.jpg');
+
+    //     await storageRef.putFile(_selectedImage!);
+    //     final imageUrl = await storageRef.getDownloadURL();
+
+    //     await FirebaseFirestore.instance
+    //         .collection('users')
+    //         .doc(userCredentials.user!.uid)
+    //         .set({
+    //       'username': 'to be done///',
+    //       'username': _enteredUsername,
+    //       'email': _enteredEmail,
+    //       'image_url': imageUrl,
+    //     });
+    //   }
+    // } on FirebaseAuthException catch (error) {
+    //   if (error.code == 'email-already-in-use') {}
+    //   ScaffoldMessenger.of(context).clearSnackBars();
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(
+    //       content: Text(error.message ?? "Authentication failed."),
+    //     ),
+    //   );
+    //   setState(() {
+    //     _isAuthenticating = false;
+    //   });
+    // }
+  }
+
   @override
   void initState() {
     emailController = TextEditingController();
     passwordController = TextEditingController();
     super.initState();
+  }
+
+  void _navigate() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const HomeScreen(),
+      ),
+    );
   }
 
   @override
@@ -46,63 +130,103 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget _buildInputFields() {
     return ClipPath(
-      clipper: CustomClipperWidget(),
-      child: Container(
-        decoration: const BoxDecoration(
+        clipper: CustomClipperWidget(),
+        child: Container(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 79, 200, 83),
-            Colors.greenAccent,
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        )),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+              colors: [
+                Color.fromARGB(255, 79, 200, 83),
+                Colors.greenAccent,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
           child: Column(
             children: [
               const SizedBox(height: 60),
-              const Text(
-                "Sign in",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.w800,
-                  letterSpacing: 1,
-                ),
-              ),
-              const SizedBox(height: 60),
-              _buildTextField(emailController, Icons.person_outline, "Email"),
-              const SizedBox(height: 20),
-              _buildTextField(
-                  passwordController, Icons.info_outline, "Password",
-                  isPassword: true),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  //Login Button Pressed
-                  String email = emailController.text.trim();
-                  String password = passwordController.text.trim();
-                  print("Email : $email Password : $password");
-                },
-                child: const Text("LOGIN"),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Forgot Password?",
+              if (_isLogin)
+                const Text(
+                  "Sign in",
                   style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 17,
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w800,
+                    letterSpacing: 1,
                   ),
                 ),
-              )
+              if (!_isLogin)
+                const Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w800,
+                    letterSpacing: 1,
+                  ),
+                ),
+              const SizedBox(height: 60),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      _buildTextField(
+                          emailController, Icons.person_outline, "Email",
+                          isPassword: false),
+                      // if (!_isLogin)
+                      //   TextFormField(
+                      //       decoration:
+                      //           const InputDecoration(labelText: "Username"),
+                      //       enableSuggestions: false,
+                      //       validator: (value) {
+                      //         if (value == null ||
+                      //             value.isEmpty ||
+                      //             value.trim().length < 4) {
+                      //           return "Please enter atleast 4 characters.";
+                      //         }
+                      //         return null;
+                      //       },
+                      //       onSaved: (value) {
+                      //         _enteredUsername = value!;
+                      //       }),
+                      _buildTextField(
+                          passwordController, Icons.info_outline, "Password",
+                          isPassword: true),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      if (_isAuthenticating) const CircularProgressIndicator(),
+                      if (!_isAuthenticating)
+                        ElevatedButton(
+                          // onPressed: _submit,
+                          onPressed: _navigate,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                          ),
+                          child: Text(_isLogin ? "Login" : "Sign Up"),
+                        ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        },
+                        child: Text(_isLogin
+                            ? "Create an account"
+                            : "I already have an account"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   Widget _buildSocialLogins() {
@@ -143,7 +267,23 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget _buildTextField(
       TextEditingController controller, IconData icon, String hint,
       {bool isPassword = false}) {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (!isPassword && value == null ||
+            value!.trim().isEmpty ||
+            !value.contains('@')) {
+          return "Please enter a valid email address";
+        }
+        if (isPassword && value == null || value.trim().length < 6) {
+          return "Password must be atleast 6 characters long";
+        }
+      },
+      onSaved: (value) {
+        if (isPassword)
+          _enteredPassword = value!;
+        else
+          _enteredEmail = value!;
+      },
       controller: controller,
       style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
